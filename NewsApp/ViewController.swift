1099962dbde3bafaa6a0a34968ea98f9af434c8e
James Junior <james@dihardjasoftware.com>
2023-03-11 10:57:21 +0700

    feat: project init chore: update readme

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by James Junior on 11/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
